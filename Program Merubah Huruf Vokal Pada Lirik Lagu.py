1ad21e796c2f4cb9903f55a35ad0f3bac9dcafb3
SalsabillaTriPutri <162257781+SalsabillaTriPutri@users.noreply.github.com>
2024-03-29 11:20:10 +0700

    Add files via upload

---
+++ Program Merubah Huruf Vokal Pada Lirik Lagu.py
@@ -0,0 +1,25 @@
+nama = "Salsabilla Tri Putri"
+nim = "2310431017"
+shift = "2"
+
+print("\nNama  : "+nama)
+print("NIM   : "+nim)
+print("Shift : "+shift)
+
+print("\n============================")
+print("     PROGRAM LIRIK LAGU     ")
+print("============================\n")
+singer = input("Song by : ")
+title = input("Title   : ")
+vokal = 'aiueoAIUO'
+lirik = input("\nLirik : ")
+perubahan = ''
+
+for huruf in lirik:
+    if huruf in vokal:
+        perubahan += "i"
+    else:
+        perubahan += huruf
+
+print("SLirik yang telah dirobah : ",perubahan)
+print()
